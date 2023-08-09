ad641116d0e61b0d18dd3da9aecc460d352d8836
Haroon <haroondhudi555@gmail.com>
2023-08-09 14:53:56 +0500

    First

---
+++ app/src/main/java/com/example/authntation/MainActivity3.java
@@ -0,0 +1,26 @@
+package com.example.authntation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity3 extends AppCompatActivity {
+    TextView textView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+        textView=findViewById(R.id.bt1);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity3.this,MainActivity2.class));
+            }
+        });
+    }
+}
